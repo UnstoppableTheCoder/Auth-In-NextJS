630800395423eda74c254eefcccd61c0bd5bd793
Unstoppable <VipendraSinghPatel999@gmail.com>
2025-06-29 17:18:17 +0530

    Modify

--- src/app/api/users/send-verification-email/[id]/route.ts
+++ src/app/api/users/send-verification-email/[id]/route.ts
@@ -2,12 +2,14 @@ import sendEmail from "@/helpers/mailer";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest) {
   try {
-    const { id } = await params;
+    const id = request.nextUrl.pathname.split("/").pop();
+
+    if (!id) {
+      return NextResponse.json({ error: "Missing user ID" }, { status: 400 });
+    }
+
     const user = await User.findById(id);
 
     if (!user) {
@@ -20,10 +22,9 @@ export async function GET(
       { status: 200 }
     );
   } catch (error: unknown) {
-    let message = "An unknown error occurred";
-    if (error instanceof Error) {
-      message = error.message;
-    }
+    const message =
+      error instanceof Error ? error.message : "An unknown error occurred";
+
     return NextResponse.json({ error: message }, { status: 500 });
   }
 }
